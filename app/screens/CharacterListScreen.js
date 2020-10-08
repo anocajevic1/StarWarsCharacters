2f30a91fdf68cab8b5f5791c5e49f41f4932124f
Amna <anocajevic1@etf.unsa.ba>
2020-10-08 12:38:43 +0200

    Prikazuje se table favorita i mogu se uklanjati elementi iz nje.

--- app/screens/CharacterListScreen.js
+++ app/screens/CharacterListScreen.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { DataTable } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import axios from "axios";
 
 export default class CharacterListScreen extends Component {
+  state = {
+    characters: [],
+  };
+
   constructor(props) {
     super(props);
   }
 
   getCharacters() {
-    //for(int i=0; i<50; i++) ??
-    //ovo dobavljanje radi samo trebam smisliti nacin kako da ih smjestam u tabelu
-    //ako se ova tabele ne moze dirati onda moze obicni ul li
-
     axios.get("https://swapi.dev/api/people/?_limit=20").then((response) => {
       this.setState({ characters: response.data }, () => {
         console.log(this.state.characters);
@@ -27,15 +35,12 @@ export default class CharacterListScreen extends Component {
 
   render() {
     return (
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <SafeAreaView>
-          {/* long content here */}
-          <Button
-            title="View my favorites"
-            onPress={() => this.props.navigation.navigate("Favorites")}
-          ></Button>
-        </SafeAreaView>
-      </ScrollView>
+      <SafeAreaView>
+        <Button
+          title="View my favorites"
+          onPress={() => this.props.navigation.navigate("Favorites")}
+        ></Button>
+      </SafeAreaView>
     );
   }
 }
--- app/screens/MyFavoritesScreen.js
+++ app/screens/MyFavoritesScreen.js
@@ -1,17 +1,74 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-import { DataTable } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  TouchableWithoutFeedback,
+  Button,
+  Alert,
+} from "react-native";
+import { DataTable, IconButton } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default class MyFavoritesScreen extends Component {
+  state = {
+    favorites: [
+      {
+        name: "banana",
+        key: 1,
+      },
+      {
+        name: "kivi",
+        key: 2,
+      },
+    ],
+  };
+
+  removeFromFavorites(item) {
+    //ovdje ce se brisati iz flat liste
+    let arr = this.state.favorites;
+    let position = arr.indexOf(item);
+    arr.splice(position, 1);
+    this.setState({ favorites: arr });
+  }
+
   render() {
     return (
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <SafeAreaView>
-          {/* long content here */}
-          <Text>Favorite stuff here</Text>
-        </SafeAreaView>
-      </ScrollView>
+      <SafeAreaView>
+        <Text>My Favorite Characters</Text>
+        <FlatList
+          style={styles.list}
+          data={this.state.favorites}
+          renderItem={({ item }) => (
+            <View style={styles.row} key={item.key}>
+              <Text> {item.name}</Text>
+              <IconButton
+                icon="delete"
+                onPress={() =>
+                  Alert.alert(
+                    "Delete Character",
+                    "Are you sure you want to delete this character from your favorites?",
+                    [
+                      {
+                        text: "Cancel",
+                        onPress: () => console.log("Cancel Pressed"),
+                        style: "cancel",
+                      },
+                      {
+                        text: "OK",
+                        onPress: () => this.removeFromFavorites(item),
+                      },
+                    ],
+                    { cancelable: false }
+                  )
+                }
+              />
+            </View>
+          )}
+        />
+      </SafeAreaView>
     );
   }
 }
@@ -22,5 +79,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
+  },
+  row: {
+    display: "flex",
+    flexDirection: "row",
   },
 });
